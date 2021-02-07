c9f5e8fedc1f33080cb6066186dec5fd6691d3f4
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-07 18:17:15 +0200

    feat: add page routing

---
+++ src/js/views/Calendar.js
@@ -0,0 +1,21 @@
+import AbstractView from './AbstractView';
+
+class Calendar extends AbstractView {
+  constructor(params) {
+    super(params);
+    this.setTitle('Event Calendar');
+  }
+
+  create() {}
+
+  async render() {
+    return `
+        <h1>EVENT CALENDAR</h1>
+        <p>
+          <a class="list__link" href="/create-event" data-link>New event +</a>
+        </p>
+    `;
+  }
+}
+
+export default Calendar;
---
+++ src/js/views/Event.js
@@ -0,0 +1,19 @@
+import AbstractView from './AbstractView';
+
+class Event extends AbstractView {
+  constructor(params) {
+    super(params);
+    this.setTitle('Create Event');
+  }
+
+  async render() {
+    return `
+        <h1>Event!</h1>
+        <p>
+            <a href="/calendar" data-link>Calendar</a>.
+        </p>
+    `;
+  }
+}
+
+export default Event;
