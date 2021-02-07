7bea2e7dc852b8811490e01e144937eb981a1e9b
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-07 21:11:42 +0200

    fix: fix server paths

--- src/js/server.js
+++ src/js/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
 
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(
-  '/static',
-  express.static(path.resolve(__dirname, 'frontend', 'static'))
-);
+// serve static assets normally
+app.use(express.static(`${__dirname}/dist`));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('/*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '/dist', 'index.html'));
 });
+
+app.listen(port);
 // eslint-disable-next-line no-console
-app.listen(process.env.PORT || 5060, () => console.log('Server running...'));
+console.log(`server started on port + ${port}`);
