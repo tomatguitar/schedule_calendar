73d412c5dbfbe4fd3dcbdb9e08f17bfea98ef565
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-10 17:27:25 +0200

    feat: add event-form's styles

--- src/js/pageLayouts/calendarLayout.js
+++ src/js/pageLayouts/calendarLayout.js
@@ -1,7 +1,7 @@
 const calendarHeader = `
 <header class="header">
-<label for="select">Filter events by participant's name:</label>
-<div class="filter">
+  <label for="select">Filter events by participant's name:</label>
+  <div class="filter">
     <select name="filter" class="select">
       <option value=""></option>
       <option value="Maria">Maria</option>
@@ -12,10 +12,10 @@ const calendarHeader = `
       <option value="Dmitry">Dmitry</option>
     </select>
     <div class="select-icon">
-    <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">
-      <path d="m2e1 95a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm14 55h68v1e1h-68zm0-3e1h68v1e1h-68zm0-3e1h68v1e1h-68z"></path>
-    </svg>
-  </div>
+      <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">
+        <path d="m2e1 95a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm14 55h68v1e1h-68zm0-3e1h68v1e1h-68zm0-3e1h68v1e1h-68z"></path>
+      </svg>
+    </div>
   </div>
 
   <a class="button" href="/create-event" data-link>New event +</a>
--- src/js/pageLayouts/eventLayout.js
+++ src/js/pageLayouts/eventLayout.js
@@ -1,34 +1,48 @@
 const eventFormLayout = `
 <a href="/calendar" data-link>Calendar</a>
-<form action="">
-      <label for="event-name"
-        >Name of event
-        <input type="text" name="event-name" id="event-name" />
-      </label>
-      <label for="participants">
-        Participants
-        <select name="participants" id="participants">
-          <option value="">Maria</option>
-          <option value="">Oleg</option>
-          <option value="">Ivan</option>
-          <option value="">Kate</option>
-          <option value="">Andrey</option>
-          <option value="">Dmitry</option>
+<h2 class="title">Create New Event</h2>
+<form  class="event" action="">
+      <label class="event-label" for="event-name">Name of event: </label>
+      <div class="input">
+        <input type="text" name="event-name" id="event-name"/>
+      </div>
+      <label class="event-label" for="participants">Participants: </label>
+      <div class="filter">
+        <select name="participants" class="select" id="participants">
+          <option value="participants"></option>
+          <option value="Maria">Maria</option>
+          <option value="Oleg">Oleg</option>
+          <option value="Ivan">Ivan</option>
+          <option value="Kate">Kate</option>
+          <option value="Andrey">Andrey</option>
+          <option value="Dmitry">Dmitry</option>
         </select>
-      </label>
-      <label for="day">
-        Day
-        <select name="day" id="day">
+        <div class="select-icon">
+          <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">
+            <path d="m2e1 95a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm14 55h68v1e1h-68zm0-3e1h68v1e1h-68zm0-3e1h68v1e1h-68z"></path>
+          </svg>
+        </div>
+      </div>
+      <label class="event-label" for="day">Day: </label>
+      <div class="filter">
+        <select name="day" class="select" id="day">
+          <option value="0"></option>
           <option value="Monday">Monday</option>
           <option value="Tuesday">Tuesday</option>
           <option value="Wednesday">Wednesday</option>
           <option value="Thursday">Thursday</option>
           <option value="Friday">Friday</option>
         </select>
-      </label>
-      <label for="time">
-        Time
-        <select name="time" id="time">
+        <div class="select-icon">
+          <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">
+            <path d="m2e1 95a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm14 55h68v1e1h-68zm0-3e1h68v1e1h-68zm0-3e1h68v1e1h-68z"></path>
+          </svg>
+        </div>
+      </div>
+      <label class="event-label" for="time">Time: </label>
+      <div class="filter">
+        <select name="time" class="select" id="time">
+          <option value="0"></option>
           <option value="10:00">10:00</option>
           <option value="11:00">11:00</option>
           <option value="12:00">12:00</option>
@@ -39,9 +53,16 @@ const eventFormLayout = `
           <option value="17:00">17:00</option>
           <option value="18:00">18:00</option>
         </select>
-      </label>
-      <button id="cancel">Cancel</button>
-      <button id="create">Create</button>
+        <div  class="select-icon">
+          <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">
+            <path d="m2e1 95a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm14 55h68v1e1h-68zm0-3e1h68v1e1h-68zm0-3e1h68v1e1h-68z"></path>
+          </svg>
+        </div>
+      </div>
+      <div class="button-wrapper">
+        <button class="button" id="cancel">Cancel</button>
+        <button class="button" id="create">Create</button>
+      </div>
     </form>`;
 
 export default eventFormLayout;
