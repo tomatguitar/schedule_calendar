0de4ed5fa51c906d4a8d60d112037fdb3f4a4d51
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-10 19:04:10 +0200

    feat: add select's options generation method

--- src/js/views/AbstractView/AbstractView.js
+++ src/js/views/AbstractView/AbstractView.js
@@ -7,6 +7,24 @@ export default class {
     document.title = title;
   }
 
+  fillOptions(elem, constant) {
+    const select = document.querySelector(`#${elem}`);
+    const optionDefault = document.createElement('option');
+    optionDefault.value = 'current';
+    optionDefault.innerText = '';
+    select.appendChild(optionDefault);
+
+    for (let i = 0; i < constant.length; i++) {
+      if (elem === 'day') {
+        i += 1;
+      }
+      const option = document.createElement('option');
+      option.value = constant[i];
+      option.innerText = constant[i];
+      select.append(option);
+    }
+  }
+
   async render() {
     return '';
   }
