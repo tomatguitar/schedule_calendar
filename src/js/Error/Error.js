4b29596169bb1543a3b3afe605d254d447e57d03
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-11 23:19:45 +0200

    feat: add error message style

--- src/js/Error/Error.js
+++ src/js/Error/Error.js
@@ -10,8 +10,27 @@ class Error {
     this.page.prepend(errorBlock);
   }
 
-  show(message) {
+  remove() {
+    const errorBlock = document.querySelector('.error');
+    errorBlock.remove();
+  }
+
+  hide() {
+    const errorBlock = document.querySelector('.error');
+    errorBlock.style.display = 'none';
+  }
+
+  show() {
+    const errorBlock = document.querySelector('.error');
+    errorBlock.style.display = 'block';
+    errorBlock.style.display = 'visible';
+    errorBlock.style.animationPlayState = 'running';
+  }
+
+  display(message) {
     this.create(message);
+    this.show();
+    setTimeout(() => this.remove(), 2500);
   }
 }
 
